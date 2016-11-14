e893fab334fe961ff0fee531a838bf22f1815630
John KM Jung <min85da@gmail.com>
2016-11-13 23:42:50 -0500

    building backend friends controller to db

--- server/controller/friendController.js
+++ server/controller/friendController.js
@@ -1,14 +1,27 @@
 var request = require('request');
+var db = require('../models/db');
 
 //Retrieve Friend List from DB
 module.exports.retrieveFriends = function (req, res){
   console.log('GET /friend API called');
+  // db('friends').where({user_id: 1}).select('user_id2').returning('user_id2')
+  db('friends').where({user_id: 1}).select('user_id2')
 
+  .then((data) => {
+    console.log(data, ': data, retreiveFriends')
+
+    res.send(data);
+  })
+  .catch((err)=> {
+    console.log(err)
+  })
 }
 
 //addFriend Friend List to DB
 module.exports.addFriend = function (req, res){
   console.log('POST /friend API called');
+  //insert into friends table
+  // db('friends').insert({user_id:1, user_id2:4})
 
 }
 
