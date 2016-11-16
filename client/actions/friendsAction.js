28b229cab1b1cf9a41a3c6cf5c875f45d5a19f49
John KM Jung <min85da@gmail.com>
2016-11-15 21:14:37 -0500

    fix merge

--- client/actions/friendsAction.js
+++ client/actions/friendsAction.js
@@ -4,16 +4,37 @@ import axios from 'axios';
 export function retrieveFriends() {
 
   const request = axios.get('api/friend');
-  console.log('Request Line 7:', request);
+
   return {
     type: 'RETRIEVE_FRIENDS',
     payload: request
   }
 }
 
+export function addFriend(userId){
+  const request = axios.post('api/friend', {friendId:userId});
+
+  return {
+    type: 'ADD_FRIEND',
+    payload: request,
+  }
+}
+
+export function deleteFriend(userId){
+  console.log(userId, ' USERID');
+  const request = axios.delete('api/friendDelete/' + userId); //1
+
+  console.log(userId, ' deleteFriend Called')
+  return {
+    type: 'DELETE_FRIEND',
+    payload: request,
+  }
+}
+
 export function getAllUser(){
-  const request = axios.get('api/allUser');
-  console.log('Request Line 16:', request);
+  const request = axios.get('api/allUser')
+
+  console.log('Request Received, Line 37', request);
   return {
     type: 'GET_ALL_USERS',
     payload: request
@@ -21,12 +42,12 @@ export function getAllUser(){
 }
 
 
+export function callUser(){
+  // const request = axios.get('api/allUser')
 
-export function addFriend(userId){
-  // console.log(userId, ' addFriend Called')
+  console.log('Request Received, Line 48', request);
   return {
-    type: 'ADD_FRIEND',
-    payload: userId
+    type: 'CALL_USER',
+    payload: request
   }
 }
-
--- client/containers/friendsAddContainer.js
+++ client/containers/friendsAddContainer.js
@@ -3,38 +3,50 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { addFriend } from '../actions/friendsAction'
-import {retrieveFriends, getAllUser } from '../actions/friendsAction';
+import { retrieveFriends, getAllUser } from '../actions/friendsAction';
 
 class FriendAdd extends React.Component {
   constructor(props){
     super(props);
-    // console.log(props, ' friendsAddContainer Props')
-    this.state = {term: '', friend:[]};
-    this.onInputChange = this.onInputChange.bind(this);
+    console.log(props, ' friendsAddContainer Props LINE 11')
+    this.state = {notFriend:[], selectedVal:''};
     this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentWillMount() {
      this.props.getAllUser();
-     // this.setState({friend:this.state.friend});
   }
 
+
+  //Sets friend as state after component when props has been changed
   componentWillReceiveProps(nextProps) {
-        // this.setState({friend:nexstProps.friend})
-
-   this.setState({term: '', friend: nextProps.friend})
-        // console.log(nextProps.friend, 'this.state componentWillReceiveProps friendadd');
+   this.setState({notFriend: nextProps.notFriend})
+    console.log(this.state, 'componentWillReceiveProps');
   }
 
+  componentWillUpdate(nextProps, nextState) {
+    console.log(this.state, 'ComponentWillUpdate')
+  }
+
+  // invoked immediately after updating occurs
+  componentDidUpdate(prevProps, prevState) {
+       // this.setState({selectedVal:this.refs.selectValue.value})
+    console.log(this.state, ' COMPONENTDIDMOUNT')
+  }
+
+  //Add friend to database
   onFormSubmit(e){
     e.preventDefault();
-    console.log(e, )
-    //need to Add Friends to current user
-    this.props.addFriend(this.state.term)
+    // need to Add Friends to current user
+    this.props.addFriend(this.state.selectedVal)
+    this.props.retrieveFriends();
   }
 
-  onInputChange(e){
-    this.setState({term: e.target.value});
+  handleChange(e){
+    // console.log(e.target.value, ' :Selected Value')
+    let id = e.target.value;
+    this.setState( {selectedVal:id} );
   }
 
   render(){
@@ -42,26 +54,30 @@ class FriendAdd extends React.Component {
       <div>
         Available Users:
       <form onSubmit={this.onFormSubmit} className="input-group">
-
-          <select className="form-control">
-          { !this.state.friend ? 'Loading Users...' :
-            this.state.friend.map( (user, i) => {return(
-            <option value='user.id' key={user.id}>{user.email}</option>) })
+          <select onChange={this.handleChange} className="form-control">
+          {
+            !this.state.notFriend ? 'Loading Users...' :
+             this.state.notFriend.map( (user, i) => {
+              return(
+            <option  value={user.id} key={user.id}>{user.email}</option> )
+            })
           }
           </select>
 
         <span className="input-group-btn">
-          <button type="submit" className="btn btn-secondary">Add</button>
+          <button className="btn btn-primary"> Add </button>
         </span>
       </form>
       </div>
-
       )
   }
 }
 function mapStateToProps(state){
-  console.log(state.friend[0], 'AllUser except friend and curr')
-  return {friend: state.friend[0]}//Array of Object(All Users)
+  console.log(state, 'AllUser except friend and curr, container/friendsAddContainer.js')
+
+  // console.log(state.friend[0], 'AllUser except friend and curr, container/friendsAddContainer.js')
+  //  console.log(state.friend[1], 'AllUser except friend and curr, container/friendsAddContainer.js')
+  return {notFriend: state.friend[0],friend:state.friend[1]}
 }
 
 //binds action and container
