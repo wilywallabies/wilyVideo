63499203fda931509de2472417b5257d456729bc
John KM Jung <min85da@gmail.com>
2016-11-22 14:06:41 -0500

    change status => react bootstrap

--- client/containers/friendDetailContainer.js
+++ client/containers/friendDetailContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { deleteFriend, callUser, retrieveFriends,  getNonFriends } from '../actions/friendsAction';
+import { Button } from 'react-bootstrap';
 
 import webrtcModule from '../components/webrtcModule';
 
