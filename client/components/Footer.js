007f401e65ff04c4dbb79ad0b8333e9da516e9b4
John KM Jung <min85da@gmail.com>
2016-11-23 11:03:50 -0500

    added glyphicon

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
     <footer className="footer">
-        <h4>WilyVideo ©</h4>
+        <h4 >WilyVideo <i className="glyphicon glyphicon-facetime-video"></i> </h4>
         <p>test1</p>
         <p>test22</p>
     </footer>
